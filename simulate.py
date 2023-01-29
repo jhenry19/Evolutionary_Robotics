7ada0aea992ded41ff973d67383149c40c418387
jhenry19 <jhny277@gmail.com>
2023-01-29 16:43:54 -0500

    single box with a floor and gravity

--- simulate.py
+++ simulate.py
@@ -1,11 +1,16 @@
 import pybullet as p
 import time as t
+import pybullet_data
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # gets custom pybullet shapes for easier use
 
+p.setGravity(0, 0, -9.8)  # set gravity
+
+planeId = p.loadURDF("plane.urdf")  # loads floor
 p.loadSDF("box.sdf")
 for i in range(1000):
     p.stepSimulation()
-    t.sleep(1/60)
+    t.sleep(1 / 60)
     print(i)
 p.disconnect()
