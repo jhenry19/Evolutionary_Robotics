e556e4793a9b808eb592541537911c9e41dee662
jhenry19 <jhny277@gmail.com>
2023-01-29 17:31:41 -0500

    body links connected by joint

--- generate.py
+++ generate.py
@@ -4,9 +4,9 @@ length = 1
 width = 1
 height = 1
 
-x = 1 / 2
-y = 1 / 2
-z = 1 / 2
+x = 0
+y = 0
+z = 1/2
 
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
@@ -17,6 +17,9 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_Leg", parent="Torso", child="Leg", type="revolute", position=[.5, 0, 1])
+    pyrosim.Send_Cube(name="Leg", pos=[.5, y, z], size=[length, width, height])
+
     pyrosim.End()
 
 Create_World()
