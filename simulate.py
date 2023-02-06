16a98fd343483dddf40a084c386db0dfa01f1025
jhenry19 <jhny277@gmail.com>
2023-02-06 11:56:01 -0500

    sensor data stored in numpy array

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import time as t
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # gets custom pybullet shapes for easier use
@@ -9,14 +10,16 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())  # gets custom pybullet s
 p.setGravity(0, 0, -9.8)  # set gravity
 
 planeId = p.loadURDF("plane.urdf")  # loads floor
-torsoId = p.loadURDF("body.urdf") # creates robot
+torsoId = p.loadURDF("body.urdf")  # creates robot
 p.loadSDF("world.sdf")
 
-pyrosim.Prepare_To_Simulate(torsoId) # sets up sensors
-for i in range(1000):
-    p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+pyrosim.Prepare_To_Simulate(torsoId)  # sets up sensors
+backLegSensorValues = numpy.zeros(100)
 
+for i in range(100):
+    p.stepSimulation()
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     t.sleep(1 / 60)
-    print(i)
 p.disconnect()
+
+print(backLegSensorValues)
