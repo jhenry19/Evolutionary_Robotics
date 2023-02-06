2cdbbfd609da0d4ce271f4ad5911798ac237f6c2
jhenry19 <jhny277@gmail.com>
2023-02-06 11:42:58 -0500

    sensor added. No output

--- generate.py
+++ generate.py
@@ -16,11 +16,11 @@ def Create_World():
 
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
-    pyrosim.Send_Cube(name="Link0", pos=[0, 0, .5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link0_Link1", parent="Link0", child="Link1", type="revolute", position=[.5, 0, 1])
-    pyrosim.Send_Cube(name="Link1", pos=[.5, y, .5], size=[length, width, height])
-    pyrosim.Send_Joint(name="Link1_Link2", parent="Link1", child="Link2", type="revolute", position=[1, 0, 0])
-    pyrosim.Send_Cube(name="Link2", pos=[.5, y, -.5], size=[length, width, height])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0, 0, .5], size=[length, width, height])
+    pyrosim.Send_Joint(name="BackLeg_Torso", parent="BackLeg", child="Torso", type="revolute", position=[.5, 0, 1])
+    pyrosim.Send_Cube(name="Torso", pos=[.5, y, .5], size=[length, width, height])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[1, 0, 0])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[.5, y, -.5], size=[length, width, height])
 
     pyrosim.End()
 
--- simulate.py
+++ simulate.py
@@ -1,6 +1,7 @@
 import pybullet as p
 import time as t
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())  # gets custom pybullet shapes for easier use
@@ -11,8 +12,11 @@ planeId = p.loadURDF("plane.urdf")  # loads floor
 torsoId = p.loadURDF("body.urdf") # creates robot
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(torsoId) # sets up sensors
 for i in range(1000):
     p.stepSimulation()
+    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+
     t.sleep(1 / 60)
     print(i)
 p.disconnect()
