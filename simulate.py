90e5a50a95c843f1396a46dfd0f57207a581eb22
jhenry19 <jhny277@gmail.com>
2023-02-09 16:29:20 -0500

    Started adding motor

--- simulate.py
+++ simulate.py
@@ -10,17 +10,22 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())  # gets custom pybullet s
 p.setGravity(0, 0, -9.8)  # set gravity
 
 planeId = p.loadURDF("plane.urdf")  # loads floor
-torsoId = p.loadURDF("body.urdf")  # creates robot
+robotId = p.loadURDF("body.urdf")  # creates robot
 p.loadSDF("world.sdf")
 
-pyrosim.Prepare_To_Simulate(torsoId)  # sets up sensors
+pyrosim.Prepare_To_Simulate(robotId)  # sets up sensors
 backLegSensorValues = numpy.zeros(100)
 frontLegSensorValues = numpy.zeros(100)
 
 for i in range(100):
     p.stepSimulation()
+
+    # Store sensor values
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+
+    # Motor
+    pyrosim.Set_Motor_For_Joint(bodyIndex=robotId, jointName="Torso_BackLeg", controlMode=p.POSITION_CONTROL, targetPosition=0.0, maxForce=500)
     t.sleep(1 / 10)
 p.disconnect()
 
