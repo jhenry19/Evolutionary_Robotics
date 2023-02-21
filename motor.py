dc73d897950d46db2f063eedb450386400cd7e72
jhenry19 <jhny277@gmail.com>
2023-02-21 09:24:27 -0500

    finished refactoring assignment

--- motor.py
+++ motor.py
@@ -16,13 +16,22 @@ class MOTOR:
         numpy.save("data/" + self.jointName, self.motorValues)
 
     def Prepare_To_Act(self):
+        self.amplitude = c.FRONT_AMPLITUDE
+        if self.jointName == "Torso_BackLeg":
+            self.frequency = c.FRONT_FREQUENCY / 2.0
+        else:
+            self.frequency = c.FRONT_FREQUENCY
+        self.offset = c.FRONT_PHASE_OFFSET
         for i in range(c.SIMULATION_STEPS):
             self.motorValues[i] = self.amplitude * numpy.sin(self.frequency / c.SIN_DIVISOR * i + self.offset)
 
     def __init__(self, jointName):
         self.jointName = jointName
-        self.amplitude = c.FRONT_AMPLITUDE
-        self.frequency = c.FRONT_FREQUENCY
-        self.offset = c.FRONT_PHASE_OFFSET
+
+        # Initialize variables for sin curve of motor values
+        self.offset = 0
+        self.frequency = 0
+        self.amplitude = 0
         self.motorValues = numpy.zeros(c.SIMULATION_STEPS)
+
         self.Prepare_To_Act()
