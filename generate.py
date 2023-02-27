49e2e2f11c836537e936b08abb430e41bb51651f
jhenry19 <jhny277@gmail.com>
2023-02-27 12:41:28 -0500

    update hidden or motor neuron function added. Assignment step 52

--- generate.py
+++ generate.py
@@ -27,9 +27,14 @@ def Generate_Body():
 
 def Generate_Brain():
     pyrosim.Start_NeuralNetwork("brain.nndf")
+    # Sensor Neurons
     pyrosim.Send_Sensor_Neuron(name=0, linkName="Torso")
     pyrosim.Send_Sensor_Neuron(name=1, linkName="BackLeg")
     pyrosim.Send_Sensor_Neuron(name=2, linkName="FrontLeg")
+
+    # Motor Neurons
+    pyrosim.Send_Motor_Neuron(name=3, jointName="Torso_BackLeg")
+    pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
     pyrosim.End()
 
 
--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -104,4 +104,6 @@ class NEURAL_NETWORK:
         for neuronName in sorted(self.neurons):
             if self.neurons[neuronName].Is_Sensor_Neuron():
                 self.neurons[neuronName].Update_Sensor_Neuron()
+            else:
+                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron()
 
