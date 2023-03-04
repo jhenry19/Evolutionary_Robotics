c80936a263a7aec3b4a8a317332d266bdd02d857
jhenry19 <jhny277@gmail.com>
2023-03-04 09:52:38 -0500

    added synapses to neural network

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -1,19 +1,19 @@
-from pyrosim.neuron  import NEURON
+from pyrosim.neuron import NEURON
 
 from pyrosim.synapse import SYNAPSE
 
-class NEURAL_NETWORK: 
 
-    def __init__(self,nndfFileName):
+class NEURAL_NETWORK:
+
+    def __init__(self, nndfFileName):
 
         self.neurons = {}
 
         self.synapses = {}
 
-        f = open(nndfFileName,"r")
+        f = open(nndfFileName, "r")
 
         for line in f.readlines():
-
             self.Digest(line)
 
         f.close()
@@ -28,15 +28,15 @@ class NEURAL_NETWORK:
 
         print("")
 
-# ---------------- Private methods --------------------------------------
+    # ---------------- Private methods --------------------------------------
 
-    def Add_Neuron_According_To(self,line):
+    def Add_Neuron_According_To(self, line):
 
         neuron = NEURON(line)
 
-        self.neurons[ neuron.Get_Name() ] = neuron
+        self.neurons[neuron.Get_Name()] = neuron
 
-    def Add_Synapse_According_To(self,line):
+    def Add_Synapse_According_To(self, line):
 
         synapse = SYNAPSE(line)
 
@@ -44,58 +44,54 @@ class NEURAL_NETWORK:
 
         targetNeuronName = synapse.Get_Target_Neuron_Name()
 
-        self.synapses[sourceNeuronName , targetNeuronName] = synapse
+        self.synapses[sourceNeuronName, targetNeuronName] = synapse
 
-    def Digest(self,line):
+    def Digest(self, line):
 
         if self.Line_Contains_Neuron_Definition(line):
-
             self.Add_Neuron_According_To(line)
 
         if self.Line_Contains_Synapse_Definition(line):
-
             self.Add_Synapse_According_To(line)
 
-    def Line_Contains_Neuron_Definition(self,line):
+
+    def Line_Contains_Neuron_Definition(self, line):
 
         return "neuron" in line
 
-    def Line_Contains_Synapse_Definition(self,line):
+    def Line_Contains_Synapse_Definition(self, line):
 
         return "synapse" in line
 
     def Print_Sensor_Neuron_Values(self):
 
-        print("sensor neuron values: " , end = "" )
+        print("sensor neuron values: ", end="")
 
         for neuronName in sorted(self.neurons):
 
             if self.neurons[neuronName].Is_Sensor_Neuron():
-
                 self.neurons[neuronName].Print()
 
         print("")
 
     def Print_Hidden_Neuron_Values(self):
 
-        print("hidden neuron values: " , end = "" )
+        print("hidden neuron values: ", end="")
 
         for neuronName in sorted(self.neurons):
 
             if self.neurons[neuronName].Is_Hidden_Neuron():
-
                 self.neurons[neuronName].Print()
 
         print("")
 
     def Print_Motor_Neuron_Values(self):
 
-        print("motor neuron values: " , end = "" )
+        print("motor neuron values: ", end="")
 
         for neuronName in sorted(self.neurons):
 
             if self.neurons[neuronName].Is_Motor_Neuron():
-
                 self.neurons[neuronName].Print()
 
         print("")
@@ -105,7 +101,7 @@ class NEURAL_NETWORK:
             if self.neurons[neuronName].Is_Sensor_Neuron():
                 self.neurons[neuronName].Update_Sensor_Neuron()
             else:
-                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron()
+                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron(self.neurons, self.synapses)
 
     def Get_Neuron_Names(self):
         return self.neurons.keys()
@@ -118,4 +114,3 @@ class NEURAL_NETWORK:
 
     def Get_Value_Of(self, neuronName):
         return self.neurons[neuronName].Get_Value()
-
