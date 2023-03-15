fb2c42168a278890c788c2441f96a6dc4b8556fd
jhenry19 <jhny277@gmail.com>
2023-03-15 15:36:27 -0400

    weighted array created in solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+
 class SOLUTION:
     def __init__(self):
-        pass
+        self.weights = np.array([np.array([np.random.rand(), np.random.rand()]), np.array([np.random.rand(), np.random.rand()]), np.array([np.random.rand(), np.random.rand()])])
+        self.weights = self.weights * 2 - 1
