f0d833e206af07302151ed4721b94b12e77cbbe6
jhenry19 <jhny277@gmail.com>
2023-04-23 15:37:38 -0400

    created analysis file which produces a graph

--- plotFitnessValues.py
+++ plotFitnessValues.py
@@ -4,31 +4,41 @@ import constants as c
 
 
 def showGraph():
-
     ZeroHiddenFitnessValues = np.load("0FitnessResults.npy")
     OneHiddenFitnessValues = np.load("1FitnessResults.npy")
     TwoHiddenFitnessValues = np.load("2FitnessResults.npy")
     ThreeHiddenFitnessValues = np.load("3FitnessResults.npy")
-    # FourHiddenFitnessValues = np.load("4FitnessResults.npy")
+    FourHiddenFitnessValues = np.load("4FitnessResults.npy")
 
-    mat.plot(getMeanOfEvolution(ZeroHiddenFitnessValues), linewidth=1)
-    mat.plot(getMeanOfEvolution(OneHiddenFitnessValues), linewidth=2)
-    mat.plot(getMeanOfEvolution(TwoHiddenFitnessValues), linewidth=3)
-    mat.plot(getMeanOfEvolution(ThreeHiddenFitnessValues), linewidth=4)
-    # mat.plot(getMeanOfEvolution(FourHiddenFitnessValues), linewidth=5)
+    mat.plot(getMeanOfEvolution(ZeroHiddenFitnessValues), linewidth=1, label="0")
+    mat.plot(getMeanOfEvolution(OneHiddenFitnessValues), linewidth=2, label="1")
+    mat.plot(getMeanOfEvolution(TwoHiddenFitnessValues), linewidth=3, label="2")
+    mat.plot(getMeanOfEvolution(ThreeHiddenFitnessValues), linewidth=4, label="3")
+    mat.plot(getMeanOfEvolution(FourHiddenFitnessValues), linewidth=5, label="4")
+
+    mat.xticks(x)
+
+    mat.xlabel("Generations")
+    mat.ylabel("Fitness")
+    mat.grid(axis='x', color='0.95')
+    mat.legend()
+    mat.title("Average Fitness with Changing Number of Hidden Neuron")
     mat.show()
 
 
 def getMeanOfEvolution(array):
     toMean = []
     means = []
-    for i in range(c.NUMBER_OF_GENERATIONS):
+    for x in xValues:
         for value in range(len(array)):
-            toMean.append(array[value][i])
+            toMean.append(array[value][x])
         means.append(np.mean(toMean))
         toMean.clear()
 
     return means
 
-
+# Set tick marks at each generation
+xValues = []
+for i in range(c.NUMBER_OF_GENERATIONS):
+    x.append(i)
 showGraph()
